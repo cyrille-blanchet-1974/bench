4c747268ffab039a83592cff157f76aefa7c0c0b
Cyrille Blanchet <cyrille.blanchet@gmail.com>
2020-08-23 17:58:42 +0200

    maj timings

--- src/main.rs
+++ src/main.rs
@@ -4,100 +4,54 @@ use std::time::SystemTime;
 
 
 /*
- 1 millions de boucles
+ 1000 milliard de boucles
  1 add + 1 cmp pour boucle
  1 add pour op
  1 sub pour step
  1 cmp
 => par itération 2 add 2 cmp 1 cmp a 1 op/cycles
-=> 5 millions de cycles 
+=> 5000 milliard de cycles 
+=> 5 000 secondes à 1Ghz ?
 
-
-chomebook
-m3y8100 frq normale 1.1Ghz turbo 3.4Ghz
-2 coeurs + HT
+Machines de tests:
+ASUS chomebook => m3 8100Y 
+  https://ark.intel.com/content/www/fr/fr/ark/products/185282/intel-core-m3-8100y-processor-4m-cache-up-to-3-40-ghz.html 
+  2C/4T 1.1Ghz Base 3.4Ghz turbo (1 coeur ?) 14nm Q3 2018 4Mo de cache 4 watt TDP UHD 615 900Mhz
+DELL XPS17 => i7 2670QGM 
+  https://ark.intel.com/content/www/fr/fr/ark/products/53469/intel-core-i7-2670qm-processor-6m-cache-up-to-3-10-ghz.html 
+  4C/8T 2.2Ghz Base 3.1ghz turbo (1 coeur ?) 32nm Q4 2011 6Mo de cache 45 watt TDP  HD 3000 1100Mhz
+HP zbook => i7-6820HK
+  https://ark.intel.com/content/www/fr/fr/ark/products/88970/intel-core-i7-6820hq-processor-8m-cache-up-to-3-60-ghz.html 
+  4C/8T 2.7Ghz Base 3.6Ghz turbo (1 coeur ?) 14nm Q3 2015 8Mo de cache 45 watt TDP HD 530 1050Mhz
 ----------------------------------------
-XPS17
-core i7 2670QM  2.2Ghz turbo à 3.1
-4 coeurs + HT
-********************************************************************
-1 thread (turbo max normalement)
+théorie
+1 Thread pour 5 000 milliards d'op
+2 Thread pour 2 500 milliards d'op
+4 Thread pour 1 250 milliards d'op -> 1250s@1Ghz -> 625s @ 2Ghz (CB)
+8 Thread pour 625 milliards d'op -> 625s @ 1Ghz -> 284s @2.2Ghz (XPS)
+8 Thread pour 625 milliards d'op -> 625s @ 1Ghz -> 195s @3.2Ghz (HP)
 ----------------------------------------
-CB => monte un peu a 3Ghz et 2.7Ghz la pluspart du temps
-Thread 0-> 40.108364712s
-All Threads -> 40.108628903s
+chromebook/m3
+1 Thread -> 404.575073961s (2.7Ghz tout du long)
+2 Thread ->
+4 Thread -> 241.076748227s (2.7Ghz-> 2.0)  => 2.56 ins/cycle
+8 Thread -> 261.777100944s (2.7Ghz-> 2.0)   8T plus lent car cpu a 4T seulement
 ----------------------------------------
-XPS17 => 3.06Ghz tout du long
-Thread 0-> 55.1589519s
-All Threads -> 55.1619514s
+XPX/i7
+1 Thread -> 
+2 Thread -> 342.6585474s (2.98/2.97->2.96->2.95Ghz)
+4 Thread -> 207.5616935s (2.8->2->3 sur la fin)
+8 Thread -> 218.8510899ss (2.8Ghz-> 2.2Ghz) => 1.3 IPC
 ----------------------------------------
-CB plus rapide (72.7% du temps)
-********************************************************************
-2 threads 
+HP/i7
+1 Thread -> 
+2 Thread -> 373.3341977s (3.3Ghz)
+4 Thread -> 216.979901s (3.2GHz)
+8 Thread -> 168.1269106s (3.2Ghz) => 1.15 IPC
 ----------------------------------------
-CB => cpus a 2,7ghz
-Thread 0-> 48.405346159s
-Thread 1-> 48.814498652s
-All Threads -> 48.816010449s
-----------------------------------------
-xps17 => 2.8Ghz
-Thread 0-> 58.981869s
-Thread 1-> 59.3266559s
-All Threads -> 59.3316533s
-----------------------------------------
-CB plus rapide (82.2% du temps)
-********************************************************************
-4 threads 
-----------------------------------------
-CB =>  cpus à 2.2Ghz
-Thread 3-> 96.401347831s
-Thread 0-> 97.867943323s
-Thread 2-> 98.167467206s
-Thread 1-> 98.31584045s
-All Threads -> 98.316695124s
-----------------------------------------
-XPS17 => 2.19Ghz
-Thread 2-> 72.3784986s
-Thread 1-> 72.7312846s
-Thread 3-> 73.2489611s
-Thread 0-> 76.541591s
-All Threads -> 76.5495863s
-----------------------------------------
-CB plus lent (128.44% du temps)   de plus CB = 201.41% du temps de 2 threads 
-plus de 2 fois plus lent pour le double de traitement
-********************************************************************
-8 threads 
-----------------------------------------
-CB => non lancé
-----------------------------------------
-xps17 => 2.19Ghz
-Thread 2-> 129.4158752s
-Thread 5-> 129.5938489s
-Thread 6-> 129.6553475s
-Thread 1-> 129.7082177s
-Thread 4-> 131.8195778s
-Thread 0-> 132.348947s
-Thread 3-> 132.4709454s
-Thread 7-> 132.5909433s
-All Threads -> 132.6019321s
-----------------------------------------
-XPS17 = 173.24% du temps de 4 threads
-73% plus lent pour le double de traitement
-********************************************************************
-
-release/chromebook/8T->1.952758666s
-release/chromebook/4T->952.447168ms
-release/chromebook/2T->502.97295ms
-release/chromebook/1T->434.80758ms
-LOOP*1000 divisé entre les threads
-release/chromebook/1T->404.575073961s (2.7Ghz tout du long)
-release/chromebook/2T->
-release/chromebook/4T->241.076748227s (2.7Ghz-> 2.0)
-release/chromebook/8T->261.777100944s (2.7Ghz-> 2.0) 
-
 */
 
-pub const THREAD_COUNT: i64 = 8;
+pub const THREAD_COUNT: i64 = 1;
 pub const LOOP_COUNT: i64 = 1_000_000_000_000;
 pub const STEPS: i64 = 25;
 
